4f4b3fac209dbdb8373fe98acbe97a057b9dbff1
Abhijeet Prasad <abhi.prasad16@gmail.com>
2020-04-10 06:46:52 +0530

    Integers

---
+++ Basic Data Types/integers.go
@@ -0,0 +1,9 @@
+package main
+
+func main() {
+	var x int32 = 1
+	var y int16 = 2
+	// We need Type conversion to perform Comparison among above two variables.
+	x = int32(y) // Conversion to int32
+
+}
