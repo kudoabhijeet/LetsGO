ca3bb8f1f5045d6dc55f502f133a86e9c68cffd3
Abhijeet Prasad <abhi.prasad16@gmail.com>
2020-04-10 06:37:39 +0530

    Comments in GO

---
+++ Printing/Comments.go
@@ -0,0 +1,14 @@
+package main
+
+func main() {
+	//Single Line Comments
+	var x int = 10 // Initializing value of varibale 'x' to 10
+	/*
+		Block Comments
+		Multi
+		Line
+		Comment
+	*/
+	var a int = 15 // Initializing value of varibale 'a' to 15
+
+}
