675154d2031e4f6a249ac4ef0c4c1dcd57bd59bd
Abhijeet Prasad <abhi.prasad16@gmail.com>
2020-04-10 06:40:50 +0530

    Formatting Print Statements in GO

---
+++ Printing/printing.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "Abhijeet"
+	// Basic Printing of Strings
+	fmt.Printf("Hi " + name)
+	// %s = Conversion Character for String
+	fmt.Printf("Hi %s", name) // String Formatting
+}
