dc3c453d656a99594348b45416e6d5fc8fea2d92
Abhijeet Prasad <abhi.prasad16@gmail.com>
2020-04-10 06:57:38 +0530

    Strings

---
+++ Basic Data Types/strings.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	//UTF-8 Codes are same as ASCII
+	// CodePoint is called Rune in GO
+	// You cannot modify and existing String
+	x := "Hello World" // String Literal = String with Double Quotes
+	fmt.Printf(x)
+
+}
