d0ee5e872c4553715d3d81fed7aaa4bf9084b0fb
Abhijeet Prasad <abhi.prasad16@gmail.com>
2020-04-10 07:08:09 +0530

    String Packages

---
+++ String Packages/strPackages.go
@@ -0,0 +1,38 @@
+package main
+
+func main() {
+	// Set of boolean functions to test categories of Codepoints (Runes)
+	/*
+		1. IsDigit(r rune)
+		2. IsSpace(r rune)
+		3. IsLetter(r rune)
+		4. IsLower(r rune)
+		5. IsPunct(r rune)
+	*/
+	// Set of Conversion Functions
+	/*
+		1. ToUpper(r rune)
+		2. ToLower(r rune)
+	*/
+	// Strings Package : functions to manipulate strings
+	/*
+		Search Functions :
+		1. Compare(a,b) 0 if a==b
+		2.Contains(s,substring) true/false
+		3. HasPrefix(s,prefix) true/false
+		4. Index(s,substr) returns the index of first instance of substr in s
+
+		Manipulation Functions :
+		1. Replace(s,old,new,n) n instances
+		2. ToLower(s)
+		3. ToUpper(s)
+		4. TrimSpace(s) removes whitespace
+	*/
+	/*
+		Strconv Package :
+		1. Atoi(s) str to int
+		2. Itoa(s) int to str
+		3. FormatFloat(f,fmt,prec,bitSize) Float to Str
+		4. ParseFloat (s, bitSize) Str to Float
+	*/
+}
