b36c2f9a325083790d4253d3df102dd7f9701c6e
Abhijeet Prasad <abhi.prasad16@gmail.com>
2020-04-13 06:21:01 +0530

    Length and Capacity of Slice

--- Composite Data Types/Slices.go
+++ Composite Data Types/Slices.go
@@ -1,11 +1,15 @@
-package main  
+package main
+
 import "fmt"
 
-func main (){
-	arr := [...]string{"a","b","c","d","e","f","g","h"}
+func main() {
+	arr := [...]string{"a", "b", "c", "d", "e", "f", "g", "h"}
 
-	s1:= arr[1:3]
-	s2:= arr[2:5]
+	s1 := arr[1:3]
+	s2 := arr[2:5]
 	fmt.Println(s1)
 	fmt.Println(s2)
+	// Length and Capacity
+	fmt.Print(len(s1), cap(s1))
+
 }
