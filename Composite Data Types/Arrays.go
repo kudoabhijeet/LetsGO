8ad27fd88d59225b82316a8fb32c5dfe17330e77
Abhijeet Prasad <abhi.prasad16@gmail.com>
2020-04-13 05:39:57 +0530

    Iterating through an Array

--- Composite Data Types/Arrays.go
+++ Composite Data Types/Arrays.go
@@ -5,9 +5,14 @@ import "fmt"
 func main() {
 	var x [5]int
 	x[0] = 2
-	fmt.Print(x[0])
+	// fmt.Print(x[0])
 	//Array Literals
-	var y [5]int = [5]{2,3,4,6,7}
+	var y [5]int = [5]int{10, 20, 30}
 	// ... is used to represent size of array
 	// x: = [...]{1,3,4,5}
+
+	// Iterating through Arrays
+	for i, v := range y {
+		fmt.Printf("Index : %d, Value : %d \n", i, v)
+	}
 }
