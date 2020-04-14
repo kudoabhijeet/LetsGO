de7c5b700fe28ee140a71ae4eea7298e8467fef8
Abhijeet Prasad <abhi.prasad16@gmail.com>
2020-04-14 09:47:55 +0530

    Hash Tables

--- Composite Data Types/HashTables.go
+++ Composite Data Types/HashTables.go
@@ -1,9 +1,20 @@
 package main
+
 import "fmt"
 
 func main() {
-	var idMap map[string]int
-	idMap = make(map[string]int) // creates a empty map
-	//literal 
-	id := map[string]int{"joe":123}
+	//var idMap map[string]int
+	//idMap = make(map[string]int) // creates a empty map
+	//literal
+	id := map[string]int{"joe": 123}
+	//Accessing value
+	fmt.Print(id["joe"])
+	// Updating/ assigning
+	id["jane"] = 456
+	//deleting a pair
+	delete(id, "joe")
+	//iterating through map
+	for key, val := range id {
+		fmt.Println(key, val)
+	}
 }
